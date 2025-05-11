2c9e2e1800be666f4756a839bdc23974c5004a58
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-11 16:44:49 +0200

    updated the css a bit

--- src/pages/Peek/Peek.jsx
+++ src/pages/Peek/Peek.jsx
@@ -6,10 +6,7 @@ function Peek() {
             <img
                 src="https://crafatar.com/renders/body/75c2128d-ce64-4a4f-99e0-1d950a577394"
                 alt="Logo"
-                style={{
-                    /* glow following non-transparent pixels */
-                    filter: 'drop-shadow(0 0 10px rgba(0, 200, 255, 0.8))'
-                }}
+                className="filter drop-shadow-[0_0_10px_rgba(255,255,255,0.6)]"
             />
 
             <BeakerIcon />
