d0aa68a8d88104750bfb4742088d9e7ae310ff1e
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-11 16:21:16 +0200

    removed bootstrap

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,8 @@
-import { Card } from 'react-bootstrap'
-
 function Home() {
   return (
-    <Card className="text-center">
-      <Card.Body>
-        <Card.Title>Home Page</Card.Title>
-        <Card.Text>Welcome to the Home page using React Hash Router!</Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+        <h1>SKINPEEK</h1>
+    </>
   )
 }
 
--- src/pages/Peek/Peek.jsx
+++ src/pages/Peek/Peek.jsx
@@ -1,15 +1,16 @@
-import { Card, Button } from 'react-bootstrap'
-
 function Peek() {
-  return (
-    <Card className="text-center">
-      <Card.Body>
-        <Card.Title>Peek Page</Card.Title>
-        <Card.Text>This is the Peek page. Use it to peek around!</Card.Text>
-        <Button variant="primary">Do Something</Button>
-      </Card.Body>
-    </Card>
-  )
+    return (
+        <>
+            <img
+                src="https://crafatar.com/renders/body/75c2128d-ce64-4a4f-99e0-1d950a577394"
+                alt="Logo"
+                style={{
+                    /* glow following non-transparent pixels */
+                    filter: 'drop-shadow(0 0 10px rgba(0, 200, 255, 0.8))'
+                }}
+            />
+        </>
+    )
 }
 
 export default Peek
