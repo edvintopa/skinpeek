76de9e8fef8291162c6e584da2ab7a851febc1e1
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-12 11:12:53 +0200

    moved out localStorage logic to page

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,50 +1,35 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { ArrowLeftCircleIcon, StarIcon } from "@heroicons/react/24/outline"
-import { StarIcon as StarIconSolid } from "@heroicons/react/24/solid"
-import { star, unStar, isStarred } from "../../utils/localStorageUtils"
+import { ArrowLeftCircleIcon, StarIcon }     from "@heroicons/react/24/outline"
+import { StarIcon as StarIconSolid }          from "@heroicons/react/24/solid"
 
-function ActionBar({ player }) {
-    // Load localStorage and check if the user is starred
-    const [starred, setStarred] = useState(() => isStarred(player))
-    const navigate = useNavigate();
+function ActionBar({ player, starred, onToggleStar }) {
+  const navigate = useNavigate()
 
-    const toggleStar = () => {
-        if (starred) {
-            unStar(player)
-            setStarred(false)
-        } else {
-            star(player)
-            setStarred(true)
-        }
-    }
+  return (
+    <div className="flex items-center justify-center gap-10 mt-20">
+      <ArrowLeftCircleIcon
+        onClick={() => navigate("/")}
+        className="w-10 cursor-pointer hover:text-neutral-500"
+        title="Go back"
+      />
 
-    return (
-        <div className="flex items-center justify-center gap-10 mt-20">
+      <p className="text-4xl font-black">{player}</p>
 
-            <ArrowLeftCircleIcon
-                onClick={() => navigate("/")}
-                className="w-10 cursor-pointer hover:text-neutral-500"
-                title="Go back"
-            />
-
-            <p className="text-4xl font-black">{player}</p>
-
-            {starred ? (
-                <StarIconSolid
-                    onClick={toggleStar}
-                    className="w-10 cursor-pointer text-yellow-400 hover:text-yellow-500"
-                    title={`Unfavourite ${player}`}
-                />
-            ) : (
-                <StarIcon
-                    onClick={toggleStar}
-                    className="w-10 cursor-pointer hover:text-neutral-500"
-                    title={`Favourite ${player}`}
-                />
-            )}
-        </div>
-    )
+      {starred ? (
+        <StarIconSolid
+          onClick={onToggleStar}
+          className="w-10 cursor-pointer text-yellow-400 hover:text-yellow-500"
+          title={`Unfavourite ${player}`}
+        />
+      ) : (
+        <StarIcon
+          onClick={onToggleStar}
+          className="w-10 cursor-pointer hover:text-neutral-500"
+          title={`Favourite ${player}`}
+        />
+      )}
+    </div>
+  )
 }
 
 export default ActionBar
--- src/pages/Peek/Peek.jsx
+++ src/pages/Peek/Peek.jsx
@@ -2,19 +2,35 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ActionBar from "../../components/ActionBar/ActionBar"
 import SkinView from "../../components/SkinView/SkinView"
+import { star, unStar, isStarred } from "../../utils/localStorageUtils"
 
 function Peek() {
-    const [uuid, setUuid] = useState("75c2128d-ce64-4a4f-99e0-1d950a577394")
     const { player } = useParams()
+    const [starred, setStarred] = useState(() => isStarred(player))
+    const [uuid, setUuid] = useState("75c2128d-ce64-4a4f-99e0-1d950a577394")
 
     useEffect(() => {
         //get player UUID placeholder
     }, [player])
 
+    const toggleStar = () => {
+    if (starred) {
+      unStar(player)
+      setStarred(false)
+    } else {
+      star(player, uuid)
+      setStarred(true)
+    }
+  }
+
     return (
         <>
             <SkinView playerUUID={uuid} />
-            <ActionBar player={player} />
+            <ActionBar
+                player={player}
+                starred={starred}
+                onToggleStar={toggleStar}
+            />
         </>
     )
 }
