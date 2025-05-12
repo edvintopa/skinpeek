b40def0e391fb483a576e3477429fd14d3e2c57a
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-12 11:42:42 +0200

    conditional render if UUID is present in localstorage

--- src/pages/Peek/Peek.jsx
+++ src/pages/Peek/Peek.jsx
@@ -2,26 +2,34 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ActionBar from "../../components/ActionBar/ActionBar"
 import SkinView from "../../components/SkinView/SkinView"
-import { star, unStar, isStarred } from "../../utils/localStorageUtils"
+import { star, unStar, isStarred, getUuid } from "../../utils/localStorageUtils"
 
 function Peek() {
     const { player } = useParams()
     const [starred, setStarred] = useState(() => isStarred(player))
-    const [uuid, setUuid] = useState("75c2128d-ce64-4a4f-99e0-1d950a577394")
+    const [uuid, setUuid] = useState("75c2128d-ce64-4a4f-99e0-1d950a577394")    //remove later
 
     useEffect(() => {
-        //get player UUID placeholder
+
+        if (starred) {
+            setUuid(getUuid(player));
+        } else {
+            // use api
+        }
+
     }, [player])
 
     const toggleStar = () => {
-    if (starred) {
-      unStar(player)
-      setStarred(false)
-    } else {
-      star(player, uuid)
-      setStarred(true)
+
+        if (starred) {
+            unStar(player)
+            setStarred(false)
+        } else {
+            star(player, uuid)
+            setStarred(true)
+        }
+        
     }
-  }
 
     return (
         <>
