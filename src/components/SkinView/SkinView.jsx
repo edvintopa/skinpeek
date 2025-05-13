72900872cfc7ce553b7cae2b3cbb56b0dc783516
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-13 15:02:21 +0200

    added hover effect and transition times

--- src/components/SkinView/SkinView.jsx
+++ src/components/SkinView/SkinView.jsx
@@ -18,19 +18,40 @@ function SkinView({ playerUUID }) {
             <img
                 src={`https://crafatar.com/avatars/${playerUUID}`}
                 alt="Logo"
-                className="filter drop-shadow-[0_0_10px_rgba(0,0,0,0.6)] dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)] w-32"
+                className="
+                    filter w-32
+                    drop-shadow-[0_0_10px_rgba(0,0,0,0.6)]
+                    dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]
+                    transition-all duration-200 ease-in-out
+                    hover:drop-shadow-[0_0_50px_rgba(0,0,0,0.8)]
+                    dark:hover:drop-shadow-[0_0_50px_rgba(255,255,255,0.5)]
+                    "
             />
 
             <img
                 src={`https://crafatar.com/renders/body/${playerUUID}`}
                 alt="Logo"
-                className="filter drop-shadow-[0_0_10px_rgba(0,0,0,0.6)] dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]"
+                className="
+                    filter
+                    drop-shadow-[0_0_10px_rgba(0,0,0,0.6)]
+                    dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]
+                    transition-all duration-200 ease-in-out
+                    hover:drop-shadow-[0_0_50px_rgba(0,0,0,0.8)]
+                    dark:hover:drop-shadow-[0_0_50px_rgba(255,255,255,0.5)]
+                    "
             />
 
             <img
                 src={`https://crafatar.com/renders/head/${playerUUID}`}
                 alt="Logo"
-                className="filter drop-shadow-[0_0_10px_rgba(0,0,0,0.6)] dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)] w-32"
+                className="
+                    filter w-32
+                    drop-shadow-[0_0_10px_rgba(0,0,0,0.6)]
+                    dark:drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]
+                    transition-all duration-200 ease-in-out
+                    hover:drop-shadow-[0_0_50px_rgba(0,0,0,0.8)]
+                    dark:hover:drop-shadow-[0_0_50px_rgba(255,255,255,0.5)]
+                    "
             />
         </div>
     )
